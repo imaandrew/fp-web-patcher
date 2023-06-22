8ca1c467c97cd17e95dbd701d85097dddef856a2
imaandrew <14946103+imaandrew@users.noreply.github.com>
2023-06-22 17:58:36 -0400

    Changes for publishing

--- src/bin/bin.rs
+++
@@ -1,9 +0,0 @@
-use fp_web_patcher::n64_decode;
-use std::env;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let rom = std::fs::read(args.get(1).unwrap()).unwrap();
-    let patch = std::fs::read(args.get(2).unwrap()).unwrap();
-    std::fs::write(args.get(3).unwrap(), n64_decode(rom, patch).unwrap()).unwrap();
-}
